9d3aeacb810a303481f452c01ad85847102df55f
Gustavo F Oliveira <hi.gustavo.oliveira@gmail.com>
2017-01-17 22:02:09 -0200

    Add Enemy

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public int health = 2;
+
+	void OnCollisionEnter2D(Collision2D theCollision) {
+
+		Debug.Log ("Hit" + theCollision.gameObject.name);
+
+		if (theCollision.gameObject.name.Contains ("Laser")) {
+			LaserBehaviour laser = theCollision.gameObject.GetComponent ("LaserBehaviour") 
+				as LaserBehaviour;
+
+			health -= laser.damage;
+			Destroy (theCollision.gameObject);
+		}
+
+		if (health <= 0) {
+
+			Destroy (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
